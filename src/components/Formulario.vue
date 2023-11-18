8b038eacbcca16d2e525856cc9863ae99dd8f2c7
Diego Santos <diegantos8@gmail.com>
2023-11-18 08:57:06 +0100

    form and first classes

---
+++ src/components/Formulario.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade pacientes y
+      <span class="text-indigo-600 font-bold">Adminístralos</span>
+    </p>
+
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+
+    </form>
+  </div>
+</template>
+<script setup></script>
