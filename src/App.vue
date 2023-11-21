ad0fbfb715054ce54529cc5d70eb240b4ec11bd8
Diegantos <diegantos8@gmail.com>
2023-11-21 17:32:23 +0100

    form pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
+  import { ref } from 'vue';
+
+  const pacientes = ([])
+
 </script>
 
 <template>
@@ -9,6 +13,12 @@
     
     <div class="mt-12 md:flex">
       <Formulario />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+        <div v-if="pacientes.length > 0"></div>
+        <div v-else class="mt-20 text-2xl text-center">No hay pacientes</div>
+      </div>
     </div>
   </div>
 </template>
