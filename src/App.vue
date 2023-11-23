c837111c10e7f99a633a13f32ca028decd7a07eb
Diegantos <diegantos8@gmail.com>
2023-11-23 09:55:54 +0100

    card info

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,11 @@
 
         <div v-if="pacientes.length > 0">
 
+              <p class="text-lg mt-5 text-center mb-10">
+                Información de
+                <span class="text-indigo-600 font-bold">Pacientes</span>
+              </p>
+
           <Paciente 
             v-for="paciente in pacientes"
             :paciente="paciente"
