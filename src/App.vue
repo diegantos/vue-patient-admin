5ae20b244e9a904404b81dc3b7490a2b1846de8b
Diegantos <diegantos8@gmail.com>
2023-11-24 11:34:52 +0100

    uid

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
+  import { ref, reactive } from 'vue';
+  import { uid } from 'uid'
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
-  import { ref, reactive } from 'vue';
   import Paciente from './components/Paciente.vue'
 
   const pacientes = ref([])
 
   const paciente = reactive({
+    id: null,
     nombre: '',
     propietario: '',
     email: '',
@@ -16,7 +18,8 @@
 
   const guardarPaciente = () => {
     pacientes.value.push({
-      ...paciente
+      ...paciente,
+      id: uid()
     })
 
     Object.assign(paciente, {
